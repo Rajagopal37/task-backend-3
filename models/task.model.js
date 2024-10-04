d59580a08566e7adf36758a27d590959c0b53150
Rajagopal37 <trajagopal37@gmail.com>
2024-10-04 17:59:50 +0530

    backend done

---
+++ models/task.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  status: { type: String, enum: ['Completed', 'Not Completed'], default: 'Not Completed' },
+  assignDate: { type: Date, required: true },
+  lastDate: { type: Date, required: true },
+  userId: { type: String, required: true },
+  createdOn:{type:Date, default: new Date().getTime()},
+//   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Reference to the user
+});
+
+module.exports = mongoose.model('Task', taskSchema);
---
+++ models/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema( {
+    fullname : {type: String},
+    email : {type: String},
+    password: {type: String},
+    createdOn : { type: Date, default: new Date().getTime() }
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
